fe524dfb479de1f187b8724bd5f57a6baacf3aa2
KauaBAG <kauabruno@gmail.com>
2025-08-17 18:57:38 -0300

    Implementação da função que cria os menus recebendo como parametro um vector de strings

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include "Rastreador.hpp"
-#include "RastrVeicular.hpp"
-#include "RastrCarga.hpp"
-#include "RastrPessoal.hpp"
-#include "Alertas.hpp"
+#include "RastreadorVeicular.hpp"
+#include "RastreadorCarga.hpp"
+#include "RastreadorPessoal.hpp"
+#include "Alerta.hpp"
 #include "AlertaVelocidade.hpp"
 #include "AlertaBateria.hpp"
 #include "AlertaZona.hpp"
-#include "TipoComunicacao.hpp"
+#include "TipoDeComunicacao.hpp"
 #include "Satelital.hpp"
 #include "Gsm.hpp"
 #include "Rfid.hpp"
@@ -14,9 +14,28 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <vector>
 
 using namespace std;
 
-void main() {
-    
+int menu(const vector<string>& array) {
+    while(true) {
+        cout << "\nMenu Principal\n";
+        cout << "=============\n";
+        for (size_t i = 0; i < array.size(); ++i) {
+            cout << setw(2) << i + 1 << " - " << array[i] << endl;
+        }
+        cout << "Escolha uma opção: ";
+        int escolha;
+        cin >> escolha;
+        if (escolha > 0 && escolha <= static_cast<int>(array.size())) {
+            return escolha;
+        } else {
+            cout << "Opção inválida. Tente novamente.\n";
+        }
+    }
+}
+
+int main() {
+    return 0; 
 }
